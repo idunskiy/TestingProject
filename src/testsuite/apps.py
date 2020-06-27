a4fc2eb380a87159ad9094a752798884825cb749
Ivan <i.dunskiy@gmail.com>
2020-06-27 16:07:49 +0300

    Revert "Hometask #17 - initial"

--- src/testsuite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestsuiteConfig(AppConfig):
-    name = 'testsuite'
--- src/user_account/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    image = models.ImageField(default='pics/default.jpg', upload_to='pics')
