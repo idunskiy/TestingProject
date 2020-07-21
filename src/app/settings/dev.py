c0576a150884e48c8dbd7d41a168cd0b58e48931
Ivan <ivan@Ivans-MacBook-Pro-3.local>
2020-07-21 11:50:17 +0300

    Databases added to Prod.py

--- src/app/settings/dev.py
+++ src/app/settings/dev.py
@@ -1,5 +1,5 @@
 from app.settings.base import * # noqa
-from app.settings.components.database import * # noqa
+# from app.settings.components.database import * # noqa
 # from app.settings.components.dev_tools import * # noqa
 
 DEBUG = True
--- src/app/settings/prod.py
+++ src/app/settings/prod.py
@@ -1,4 +1,5 @@
 from app.settings.base import * # noqa
+from app.settings.components.database import * # noqa
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
