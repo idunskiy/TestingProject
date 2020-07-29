486117bd1ddf83adf89f179e0f6af5060e6c1cf7
Ivan <ivan@Ivans-MacBook-Pro-3.local>
2020-07-29 19:19:07 +0300

    Email settings added

--- src/app/settings/components/email.py
+++ src/app/settings/components/email.py
@@ -1,6 +1,9 @@
 import os
 
-EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_PORT = os.environ['EMAIL_PORT']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
